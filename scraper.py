248cf883ea7976c31b99c7b77765e885c53d388c
Chester Yap <chesterdyap@gmail.com>
2017-10-22 14:07:49 -0500

    Fix scraper save path

--- scraper.py
+++ scraper.py
@@ -5,21 +5,25 @@ import sys
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+SAVE_DIRS = {
+	'Buffett': os.getcwd().rsplit('Warren-Buffett', 1)[0] + 'Warren-Buffett/data/raw/'
+}
 
-SAVE_DIR = os.getcwd() + '/data/raw/'
-
-def scrape_buffett():
+def scrape_buffett(save_dir=SAVE_DIRS['Buffett']):
 	header = 'http://www.berkshirehathaway.com/letters/'
 	page = urlopen('http://www.berkshirehathaway.com/letters/letters.html')
 	soup = BeautifulSoup(page.read(), 'lxml')	
 	tags = soup.findAll('a', {'href' : re.compile('.*\.(html)|(pdf)')})
 	files = map(lambda tag: tag['href'], tags)
 	for file in files:
-		local_file = open(SAVE_DIR+file, 'wb')
+		local_file = open(save_dir+file, 'wb')
 		response = urlopen(header + file)
 		local_file.write(response.read())
 		local_file.close()
 
 if __name__ == '__main__':
-	if sys.argv[1] == 'buffett':
-		scrape_buffett()
+	try:
+		if sys.argv[1] == 'buffett':
+			scrape_buffett()
+	except:
+		pass
