be6eaec0ab005941b7cf65afc93154fd590d65cb
Chester Yap <chesterdyap@gmail.com>
2017-10-28 15:55:47 -0500

    Tabs

--- notebooks/Berkshire.py
+++ notebooks/Berkshire.py
@@ -31,59 +31,59 @@ r_server.ping
 ### SETUP CODE: TO MOVE ###
 
 def create_dictionary(directory):
-    for file in os.listdir(directory):
-        if file[0] != ".":
-            key = re.findall(r'\d+', file)
-            key = int("".join(key))
-            text = textract.process(directory + "/" + file)
-            r_server.set(key, text)
-    return True
-    
+	for file in os.listdir(directory):
+		if file[0] != ".":
+			key = re.findall(r'\d+', file)
+			key = int("".join(key))
+			text = textract.process(directory + "/" + file)
+			r_server.set(key, text)
+	return True
+		
 #scrape_buffett()
 #create_dictionary("/Users/ryanneal/Desktop/Warren-Buffett/data/raw")
 
 ### END SETUP CODE ###
 
 USELESS_WORDS = stopwords.words("english") + list(string.punctuation)
-    
+	
 def significant_word(word):
 	return word not in USELESS_WORDS or len(word) > 1 or word.isalpha()
-        
+		
 def create_document(year):
-    yearly_report = r_server.get(year)
-    return str(yearly_report)
+	yearly_report = r_server.get(year)
+	return str(yearly_report)
 
 def create_stems(document_text):
 	""" Returns list of all unique stems used in a given document """
 	def stem(tokens):
-        porter = nltk.PorterStemmer()
-        return [porter.stem(t) for t in tokens]
-    data = document_text.lower()
-    words = nltk.wordpunct_tokenize(data)
+		porter = nltk.PorterStemmer()
+		return [porter.stem(t) for t in tokens]
+	data = document_text.lower()
+	words = nltk.wordpunct_tokenize(data)
 	return stem(filter(significant_word, words))
 
 def get_entities(document_text):
-    sentences = nltk.sent_tokenize(document_text)
-    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
-    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
-    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+	sentences = nltk.sent_tokenize(document_text)
+	tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+	tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+	chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
 
-    def extract_entity_names(t):
-        entity_names = []
+	def extract_entity_names(t):
+		entity_names = []
 
-        if hasattr(t, 'label') and t.label:
-            if t.label() == 'NE':
-                entity_names.append(' '.join([child[0] for child in t]))
-            else:
-                for child in t:
-                    entity_names.extend(extract_entity_names(child))
+		if hasattr(t, 'label') and t.label:
+			if t.label() == 'NE':
+				entity_names.append(' '.join([child[0] for child in t]))
+			else:
+				for child in t:
+					entity_names.extend(extract_entity_names(child))
 
-        return entity_names
+		return entity_names
 
-    entity_names = []
-    for tree in chunked_sentences:
-        # Print results per sentence
-        # print extract_entity_names(tree)
+	entity_names = []
+	for tree in chunked_sentences:
+		# Print results per sentence
+		# print extract_entity_names(tree)
 
-        entity_names.extend(extract_entity_names(tree))
-    return entity_names
+		entity_names.extend(extract_entity_names(tree))
+	return entity_names
