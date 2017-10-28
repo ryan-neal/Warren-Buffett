1999b4fe5c455506b98d733e22f3fda344c862c7
ryan-neal <ryan.neal0@gmail.com>
2017-10-28 11:47:34 -0500

    added named entity recognition

--- notebooks/Berkshire.py
+++ notebooks/Berkshire.py
@@ -21,14 +21,11 @@ import pandas_datareader.data as web
 import datetime
 import seaborn as sns
 import nltk
-from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from collections import Counter
 import pprint
 import pandas_datareader.data as web
 import datetime
-
-nlp = spacy.load('en')
 
 r_server = redis.Redis("localhost")
 r_server.ping
@@ -46,12 +43,12 @@ def create_dictionary(directory):
 
 #create_dictionary("/Users/ryanneal/Desktop/Warren-Buffett/data/raw")
 
-def create_stems(year):
-    def get_year(year):
-        yearly_report = r_server.get(year)
-        return str(yearly_report)
+def create_document(year):
+    yearly_report = r_server.get(year)
+    return str(yearly_report)
 
-    data = get_year(year).lower()
+def create_stems(year):
+    data = create_document(year).lower()
     words = nltk.wordpunct_tokenize(data)
     useless_words = nltk.corpus.stopwords.words("english") + list(string.punctuation)
 
@@ -64,10 +61,39 @@ def create_stems(year):
 
     return stem(filter_words(words))
 
+def get_all_entitities(year):
+    sentences = nltk.sent_tokenize(create_document(year))
+    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+
+    def extract_entity_names(t):
+        entity_names = []
+
+        if hasattr(t, 'label') and t.label:
+            if t.label() == 'NE':
+                entity_names.append(' '.join([child[0] for child in t]))
+            else:
+                for child in t:
+                    entity_names.extend(extract_entity_names(child))
+
+        return entity_names
+
+    entity_names = []
+    for tree in chunked_sentences:
+        # Print results per sentence
+        # print extract_entity_names(tree)
+
+        entity_names.extend(extract_entity_names(tree))
+    return entity_names
+
+def count_entities(entities):
+    counter = Counter(entities)
+    return counter
 
 def count_words(stems):
     word_counter = Counter(stems)
     most_common_words = word_counter.most_common()[:10]
     return most_common_words
 
-print(count_words(create_stems(1989)))
+print(count_entities(get_all_entitities(1982)))
