3d9907b55aa0cdc31715756c9dd2996de983f454
DJ <djsm00thee3@gmail.com>
2017-10-28 15:49:21 -0500

    Fixes path issues in scraper for windows files No longer scrapes files if they have already been scraped Indentation changes

--- scraper.py
+++ scraper.py
@@ -1,36 +1,44 @@
 import re
 import os
-import sys
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 SAVE_DIRS = {
-	'Buffett': os.getcwd().rsplit('Warren-Buffett', 1)[0] + 'Warren-Buffett/data/raw/'
+    'Buffett': os.getcwd().rsplit('Warren-Buffett', 1)[0] + os.path.join('Warren-Buffett', 'data', 'raw') + os.sep
 }
 
+
 def scrape_buffett(save_dir=SAVE_DIRS['Buffett']):
-	header = 'http://www.berkshirehathaway.com/letters/'
-	page = urlopen('http://www.berkshirehathaway.com/letters/letters.html')
-	soup = BeautifulSoup(page.read(), 'lxml')	
-	tags = soup.findAll('a', {'href' : re.compile('.*\.(html)|(pdf)')})
-	
-	files = map(lambda tag: tag['href'], tags)
-	for file in files:
-		content = urlopen(header + file).read()
-		# Check if letter or directory
-		if file.endswith('.html'):
-			bs = BeautifulSoup(content, 'lxml')
-			if bs.title.string == '--IMPORTANT NOTE--':
-				file = bs.find('a', {'href' : re.compile('.*\.(pdf)')})['href']
-				content = urlopen(header + file).read()
-		local_file = open(save_dir+file, 'wb')
-		local_file.write(content)
-		local_file.close()
+    # create the directory if it does not currently exist
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    header = 'http://www.berkshirehathaway.com/letters/'
+    page = urlopen(header + '/letters.html')
+    soup = BeautifulSoup(page.read(), 'lxml')
+    tags = soup.findAll('a', {'href': re.compile('.*\.(html)|(pdf)')})
+
+    file_names = map(lambda tag: tag['href'], tags)
+    for file_name in file_names:
+        if os.path.exists(save_dir + file_name):
+            continue  # we already scraped this
+
+        content = urlopen(header + file_name).read()
+        # Check if letter or directory
+        if file_name.endswith('.html'):
+            bs = BeautifulSoup(content, 'lxml')
+            if bs.title.string == '--IMPORTANT NOTE--':
+                file_name = bs.find('a', {'href': re.compile('.*\.(pdf)')})['href']
+                content = urlopen(header + file_name).read()
+
+        local_file = open(save_dir + file_name, 'wb+')
+        local_file.write(content)
+        local_file.close()
+
 
 if __name__ == '__main__':
-	try:
-		if sys.argv[1] == 'buffett':
-			scrape_buffett()
-	except:
-		pass
+    try:
+        scrape_buffett()
+    except Exception as e:
+        print(e)
