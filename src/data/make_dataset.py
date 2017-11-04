60f8d8ee94402efa9cf9ee9e422b97b88a26f28f
ryan-neal <ryan.neal0@gmail.com>
2017-11-04 15:46:06 -0500

    added mongodb stuff

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,7 +3,8 @@ import os
 import click
 import logging
 from dotenv import find_dotenv, load_dotenv
-
+from src.data.scraper import scrape_buffett
+from src.data.load_reports import load_data
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -15,6 +16,10 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    """ if files are in there do not override """
+    scrape_buffett()
+
+    load_data()
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
