723ada2e0f2a98c36c2c0a8cb8b13a347df29ea9
Asterisk <1042203@gmail.com>
2017-11-13 19:14:36 -0600

    fix flake detected issues

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -6,20 +6,23 @@ from dotenv import find_dotenv, load_dotenv
 from src.data.scraper import scrape_buffett
 from src.data.load_reports import load_data
 
+
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
 @click.argument('output_filepath', type=click.Path())
 def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """
+    Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
-    """ if files are in there do not override """
+    # if files are in there do not override
     scrape_buffett()
 
     load_data()
+
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
