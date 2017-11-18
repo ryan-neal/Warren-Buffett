d7c38ef090046248744bbfba0de7c76043260eca
Chester Yap <chesterdyap@gmail.com>
2017-11-18 12:50:45 -0600

    Refactor noun chunking to chunk different phrases depending on expression parameter

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -33,119 +33,131 @@ db = client['BerkshireHathaway']['reports']
 USELESS_WORDS = stopwords.words("english") + list(string.punctuation)
 
 def create_document(year):
-    """ Given a year, query MongoDB for the corresponding report """
-    return db.find_one({'year':str(year)})['text'].decode('utf-8')
+		""" Given a year, query MongoDB for the corresponding report """
+		return db.find_one({'year':str(year)})['text'].decode('utf-8')
 
 def significant_word(word):
-    return word not in USELESS_WORDS and len(word) > 1# and word.isalpha()
+		return word not in USELESS_WORDS and len(word) > 1# and word.isalpha()
 
 def clean_document(document_text):
-    pass
+		pass
 
 def get_stems(document_text):
-    """ Given the raw text of a document, return list of all unique stems """
+		""" Given the raw text of a document, return list of all unique stems """
 
-    def stem(tokens):
-        porter = nltk.PorterStemmer()
-        return [porter.stem(t) for t in tokens]
+		def stem(tokens):
+				porter = nltk.PorterStemmer()
+				return [porter.stem(t) for t in tokens]
 
-    data = document_text.lower()
-    words = nltk.word_tokenize(data)
-    return stem(filter(significant_word, words))
+		data = document_text.lower()
+		words = nltk.word_tokenize(data)
+		return stem(filter(significant_word, words))
 
 
 def get_entities(document_text, entity_type=None):
-    """ Given the raw text of a document, returns all named entities as a
-        list of tuples in the form of (NE type, entity_name)
-     """
+		""" Given the raw text of a document, returns all named entities as a
+				list of tuples in the form of (NE type, entity_name)
+		 """
 
-    def chunk(text):
-        return nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text)))
+		def chunk(text):
+				return nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(text)))
 
-    chunked_document = chunk(document_text)
-    entity_trees = filter(lambda word: type(word) is not tuple, chunked_document)
-    entity_names = [(word.label(), ' '.join([child[0] for child in word]))
-                    for word in entity_trees]
+		chunked_document = chunk(document_text)
+		entity_trees = filter(lambda word: type(word) is not tuple, chunked_document)
+		entity_names = [(word.label(), ' '.join([child[0] for child in word]))
+										for word in entity_trees]
 	
 	# Optional: Filter entities based on type
-    if entity_type:
-    	return [e for e in entity_names if e[0].lower() == entity_type.lower()]
-    
-    return entity_names
+		if entity_type:
+			return [e for e in entity_names if e[0].lower() == entity_type.lower()]
+		
+		return entity_names
 
 def get_word_count(document_text):
-    word_token = nltk.word_tokenize(document_text)
-    word_token = [word for word in word_token if word.isalpha()]
-    return (len(word_token))
+		word_token = nltk.word_tokenize(document_text)
+		word_token = [word for word in word_token if word.isalpha()]
+		return (len(word_token))
 
 def get_average_word_length(document_text):
-    words = nltk.word_tokenize(document_text)
-    filtered_words = list(filter(lambda word: word.isalpha(), words))
-    return len(''.join(filtered_words)) / len(filtered_words)
-    
+		words = nltk.word_tokenize(document_text)
+		filtered_words = list(filter(lambda word: word.isalpha(), words))
+		return len(''.join(filtered_words)) / len(filtered_words)
+		
 def get_sentence_count(document_text):
 	return len(nltk.sent_tokenize(document_text))
 
 def get_market_returns():
-    '''Getting Berkshire & SP 500 returns'''
-    start_date = date(1979, 12, 31)
-    end_date = date(2016, 12, 31)
-    y_bk = dr('BRK-A', 'yahoo', start=start_date)
-    y_bk = y_bk['Adj Close']
-    y_sp = dr('^SP500TR', 'yahoo', start=start_date) #TODO: Get longer backfill
-    y_sp = y_sp['Adj Close']
-    ts = pd.concat([y_bk, y_sp], axis=1)
-    ts.columns.values[0] = 'BRK-A'
-    ts.columns.values[1] = 'SP500TR'
-    dates_ann = pd.date_range(start_date, end_date, freq='A')
-    ts_annualret = ts.reindex(dates_ann, method='ffill').pct_change()
-    ts_annualret.pct_change()
+		'''Getting Berkshire & SP 500 returns'''
+		start_date = date(1979, 12, 31)
+		end_date = date(2016, 12, 31)
+		y_bk = dr('BRK-A', 'yahoo', start=start_date)
+		y_bk = y_bk['Adj Close']
+		y_sp = dr('^SP500TR', 'yahoo', start=start_date) #TODO: Get longer backfill
+		y_sp = y_sp['Adj Close']
+		ts = pd.concat([y_bk, y_sp], axis=1)
+		ts.columns.values[0] = 'BRK-A'
+		ts.columns.values[1] = 'SP500TR'
+		dates_ann = pd.date_range(start_date, end_date, freq='A')
+		ts_annualret = ts.reindex(dates_ann, method='ffill').pct_change()
+		ts_annualret.pct_change()
 
-    return ts_annualret
+		return ts_annualret
 
 def freq_words(document_text):
-    freqdist = nltk.FreqDist()
+		freqdist = nltk.FreqDist()
 
-    for word in nltk.word_tokenize(document_text):
-        if significant_word(word):
-            freqdist[word.lower()] += 1
-    return freqdist
+		for word in nltk.word_tokenize(document_text):
+				if significant_word(word):
+						freqdist[word.lower()] += 1
+		return freqdist
 
 def get_tags(document_text):
-    tokenized_sentence = nltk.sent_tokenize(document_text)
-    tokenized_words = [nltk.word_tokenize(sentence) for sentence in tokenized_sentence]
-    tagged_words = [nltk.pos_tag(word) for word in tokenized_words]
-    return tagged_words
+		tokenized_sentence = nltk.sent_tokenize(document_text)
+		tokenized_words = [nltk.word_tokenize(sentence) for sentence in tokenized_sentence]
+		tagged_words = [nltk.pos_tag(word) for word in tokenized_words]
+		return tagged_words
+		
 
-def get_noun_phrases(document_text):
-    tagged_words = get_tags(document_text)
-    
-    new_pattern_list = [
-    	'{<NN|NNS|NNP|NNPS><IN>*<NN|NNS|NNP|NNPS>+}',
-    	'{<JJ>*<NN|NNS|NNP|NNPS><CC>*<NN|NNS|NNP|NNPS>+}',
-    	'{<JJ>*<NN|NNS|NNP|NNPS>+}'
-    ]
-    new_patterns = 'NP: ' + '\n'.join(new_pattern_list) 
-    chunker = nltk.RegexpParser(new_patterns)
-    word_tree = [chunker.parse(word) for word in tagged_words]  # Identify NP chunks
-    nps = []
-    for sent in word_tree:
-        tree = chunker.parse(sent)
-        for subtree in tree.subtrees():
-            if subtree.label() == 'NP':
-                t = subtree
-                t = ' '.join(word for word, tag in t.leaves())
-                nps.append(t)
-    return nps
+def get_phrases(document_text, expressions=[]):
+	""" Chunk phrases based on input parameters. Return list of tuples
+		in the format (phrase, containing sentence).
+	"""
+	
+	# TODO: Refactor as input parameter
+	expressions = [
+		#'{<NN|NNS|NNP|NNPS><IN>*<NN|NNS|NNP|NNPS>+}',
+		#'{<JJ>*<NN|NNS|NNP|NNPS><CC>*<NN|NNS|NNP|NNPS>+}',
+		#'{<JJ>*<NN|NNS|NNP|NNPS>+}'
+		'{<VB|VBD><VBN>+}' # Passive voice
+	]
+		
+	new_patterns = 'Phrases: ' + '\n'.join(expressions) 
+	chunker = nltk.RegexpParser(new_patterns)
+	
+	tagged_words = get_tags(document_text)
+	word_tree = [chunker.parse(word) for word in tagged_words]	# Identify NP chunks
+	phrases = []
+	
+	for sent in word_tree:
+		sentence = ' '.join(word for word, tag in sent.leaves())
+		for subtree in sent.subtrees():
+			if subtree.label() == 'Phrases':
+				phrase = ' '.join(word for word, tag in subtree.leaves())
+				phrases.append((phrase, sentence))
+								
+	return phrases
 
 def main():
-    #print(get_entities(create_document(1999), 'person'))
-    #print(get_tags(create_document(1999)))
-    #print(get_noun_phrases(create_document(1999)))
-    #print(get_stems(create_document(1999)))
-    #x = freq_words(create_document(1999))
-    #for key, value in x.items():
-    #    print(key, value)
+	print('\n'.join(': '.join(tup) 
+		for tup in get_phrases(create_document(1999))))
+	#print(create_document(1999))
+	#print(get_entities(create_document(1999), 'person'))
+	#print(get_tags(create_document(1999)))
+	#print(get_phrases(create_document(1999)))
+	#print(get_stems(create_document(1999)))
+	#x = freq_words(create_document(1999))
+	#for key, value in x.items():
+	#		 print(key, value)
 
 
 if __name__ == '__main__':
