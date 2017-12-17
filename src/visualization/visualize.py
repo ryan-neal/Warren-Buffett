124155d7924e23081733fc7c7aa2fa5c5af5cd1a
Asterisk <1042203@gmail.com>
2017-12-16 19:03:33 -0600

    broken webapp

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,5 +1,6 @@
-from bokeh.plotting import figure, show, output_file
+from bokeh.plotting import figure, show
 from src.features.build_features import get_word_count, create_document, get_sentence_count, get_brk, get_sp
+
 
 def word_count_year(all_years):
     years = []
@@ -11,15 +12,15 @@ def word_count_year(all_years):
         word_counts.append(get_word_count(create_document(year)))
         sentence_counts.append(get_sentence_count(create_document(year)))
 
-
     p1 = figure(title="Word Count Per Year")
     p1.grid.grid_line_alpha = 0.3
     p1.xaxis.axis_label = 'Year'
     p1.yaxis.axis_label = 'Words'
 
-    p1.line(years, word_counts, color="red", legend = "word counts")
+    p1.line(years, word_counts, color="red", legend="word counts")
     show(p1)
     return
+
 
 def buffett_vs_market(all_years):
     years = [all_years[0] - 1]
@@ -28,23 +29,22 @@ def buffett_vs_market(all_years):
     for year in range(len(all_years)):
         years.append(all_years[year])
         buffett.append(buffett[year] * (1 + get_brk(all_years[year])))
-        sp.append(sp[year]* (1 + get_sp(all_years[year])))
+        sp.append(sp[year] * (1 + get_sp(all_years[year])))
 
-    p2 = figure(title = "Buffett Vs the Market")
+    p2 = figure(title="Buffett Vs the Market")
     p2.grid.grid_line_alpha = 0.3
     p2.xaxis.axis_label = 'Year'
     p2.yaxis.axis_label = 'Returns'
 
-    p2.line(years, buffett, color="red", legend = "Buffett")
-    p2.line(years, sp, color="blue", legend = "Market")
+    p2.line(years, buffett, color="red", legend="Buffett")
+    p2.line(years, sp, color="blue", legend="Market")
     show(p2)
 
 
 def main():
-    #word_count_year(range(1977, 2017))
+    # word_count_year(range(1977, 2017))
     buffett_vs_market(range(1981, 2017))
+
 
 if __name__ == '__main__':
     main()
-
-
--- src/webapp/webapp/views.py
+++ src/webapp/webapp/views.py
@@ -1,4 +1,8 @@
 from flask import render_template
+from bokeh.embed import components
+from bokeh.plotting import figure
+from bokeh.resources import INLINE
+from bokeh.util.string import encode_utf8
 
 from webapp import app
 
@@ -7,3 +11,33 @@ from webapp import app
 def index():
     app.logger.warning('sample message')
     return render_template('index.html')
+
+
+@app.route('/example')
+def example():
+
+    # init a basic bar chart:
+    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+    fig = figure(plot_width=600, plot_height=600)
+    fig.vbar(
+        x=[1, 2, 3, 4],
+        width=0.5,
+        bottom=0,
+        top=[1.7, 2.2, 4.6, 3.9],
+        color='navy'
+    )
+
+    # grab the static resources
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
+
+    # render template
+    script, div = components(fig)
+    html = render_template(
+        'example.html',
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources,
+    )
+    return encode_utf8(html)
